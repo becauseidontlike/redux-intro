040a30ad86c3fb4140ce8619dfec98045340009e
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-05-29 21:59:08 +0200

    currency calculation works again

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -14,6 +14,7 @@ const accountSlice = createSlice({
     deposit(state, action) {
       // state.balance = state.balance + action.payload;
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       // state.balance = state.balance - action.payload;
@@ -38,10 +39,30 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = "";
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    console.log(data);
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
 
 export default accountSlice.reducer;
 
